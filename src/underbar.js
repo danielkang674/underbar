cd104e21260dbe1aaf90c9ce19db5c46860f2ac9
Daniel Kang <danielkang674@gmail.com>
2018-06-29 13:39:29 -0500

    Create reduce function

--- src/underbar.js
+++ src/underbar.js
@@ -135,6 +135,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    let newCollection = [];
+    for(let i = 0; i < collection.length; i++){
+      newCollection.push(iterator(collection[i]));
+    }
+    return newCollection;
   };
 
   /*
@@ -176,6 +181,25 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if(typeof collection === "string" || Array.isArray(collection)){
+      for(let i = 0; i < collection.length; i++){
+        if(accumulator === undefined){
+          accumulator = collection[i];
+        } else {
+          let temp = iterator(accumulator, collection[i]);
+          accumulator = temp !== undefined? temp : accumulator;
+        }
+      }
+    } else {
+      for(let key in collection){
+        if(accumulator === undefined){
+          accumulator = collection[key];
+        } else {
+          iterator(accumulator, collection[key]);
+        }
+      }
+    }
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
